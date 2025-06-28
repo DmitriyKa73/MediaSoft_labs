d891826090a535ab02c87dcca32e7c7c73e12e1f
DmitriyKa73 <kazarov2013@yandex.ru>
2025-06-28 20:25:16 +0400

    Добавил конструктор

--- PracticeMediaSoft/src/BankAccount.java
+++ PracticeMediaSoft/src/BankAccount.java
@@ -1,8 +1,24 @@
 import java.time.LocalDateTime;
 
 public class BankAccount {
-    String Full_name_user;
-    int balance;
-    LocalDateTime Date_open_account;
-    boolean Blocked_account;
+    private String name;
+    private int balance;
+    private LocalDateTime dateOpened;
+    private boolean isBlocked;
+
+    // конструктор
+    public BankAccount(String name) {
+        this.name = name;
+        this.balance = 0;
+        this.dateOpened = LocalDateTime.now();
+        this.isBlocked = false;
+    }
+
+        public void displayInfo() {
+        System.out.println("Имя владельца: " + name);
+        System.out.println("Баланс: " + balance);
+        System.out.println("Дата открытия: " + dateOpened);
+        System.out.println("Счёт заблокирован: " + isBlocked);
+    }
+
 }
--- PracticeMediaSoft/src/Main.java
+++ PracticeMediaSoft/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        BankAccount person = new BankAccount("Petr");
+        person.displayInfo();
     }
 }
