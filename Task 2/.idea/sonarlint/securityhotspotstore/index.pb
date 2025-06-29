772ae544e52db920394db8bae0fc4b824fa2bf29
DmitriyKa73 <kazarov2013@yandex.ru>
2025-06-29 22:24:32 +0400

    Выполнил первую задачу в Task2 (массив)

--- Task 2/.idea/sonarlint/securityhotspotstore/index.pb
+++ Task 2/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,7 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+@
+src/CarPark.java,d\2\d266f13f3c2695530f68faf7fa398b981225e550
+F
+src/Task1_Massive.java,8\d\8d3a67959f0522c647a6907f1c48d9e1cd678766
---
+++ Task 2/src/Task1_Massive.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Task1_Massive {
+
+    static int[] carYear = new int[50];
+
+    public static void setCarYear() {
+        Random random = new Random();
+        for (int i = 0; i < carYear.length; i++) {
+            carYear[i] = random.nextInt(2026 - 2000) + 2000;
+        }
+    }
+
+    public static void CarsAfter2015() {
+        System.out.println("Машины, выпущенные после 2015 года:");
+        for (int year : carYear) {
+            if (year > 2015) {
+                System.out.print(year + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void avarageCarsAge() {
+        int totalAge = 0;
+        int totalAgeAfter2015 = 0;
+        int countAfter2015 = 0;
+        int currentYear = 2025;
+
+        for (int year : carYear) {
+            int age = currentYear - year;
+            totalAge += age;
+            if (year > 2015) {
+                totalAgeAfter2015 += age;
+                countAfter2015++;
+            }
+        }
+
+        double averageAge = (double) totalAge / carYear.length;
+        double averageAgeAfter2015 = countAfter2015 > 0
+                ? (double) totalAgeAfter2015 / countAfter2015
+                : 0;
+
+        System.out.printf("Средний возраст всех авто: %.2f лет%n", averageAge);
+        System.out.printf("Средний возраст авто после 2015 года: %.2f лет%n", averageAgeAfter2015);
+    }
+}
