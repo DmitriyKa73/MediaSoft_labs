9f506148b534329408834c4145898e0e5c24dd9b
DmitriyKa73 <kazarov2013@yandex.ru>
2025-07-03 17:03:02 +0400

    Add Task3

--- Task 3/.idea/sonarlint/securityhotspotstore/index.pb
+++ Task 3/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,27 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+=
+src/Room.java,b\d\bd5f8d5843d78f7ca60afc3df46dea54e55ede80
+D
+src/EconomyRoom.java,e\c\ec72b5aa85d19515891d82f49b445498a090e9b0
+?
+src/Prices.java,c\7\c74556a021c5f1b0cb3198a5882727c3a27e6b81
+@
+src/ProRoom.java,4\e\4e3d7ccf9f239277b699a1e23385c153ee673475
+E
+src/StandardRoom.java,1\2\121f0736464561c9063e9ad11bb984d935d180b6
+@
+src/LuxRoom.java,a\e\ae947fbea1d8c0dc9ac0745a06c797b5d0a87bbc
+E
+src/UltraLuxRoom.java,8\c\8ce9990ebb8b6051ffd3683abaccf9858dfa4709
+D
+src/RoomService.java,b\d\bd24d16e67dd31a8984400d459fa0b7a13d61d54
+O
+src/RoomAlreadyReservedExc.java,6\5\65dd9b3a25b2f0fd3176a7734ed02f63cda9531e
+J
+src/SimpleRoomService.java,f\f\ff2f26f8b63bed86007027cb79f4a04c279ffc6b
+G
+src/LuxRoomService.java,1\c\1c0fed6dfb03f9aee7cee9b3a357134172e25aaa
+K
+src/LuxRoomServiceImpl.java,4\c\4c7c1c23339a929d5b39c425fe8529100c52b48b
--- Task 3/src/Main.java
+++ Task 3/src/Main.java
@@ -1,7 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        RoomService<StandardRoom> service1 = new SimpleRoomService<>();
+        StandardRoom standard = new StandardRoom(101);
+        service1.clean(standard);
+        service1.reserve(standard);
+        service1.free(standard);
+
+        System.out.println();
+
+        LuxRoomService<UltraLuxRoom> luxService = new LuxRoomServiceImpl<>();
+        UltraLuxRoom ultra = new UltraLuxRoom(202);
+        luxService.clean(ultra);
+        luxService.reserve(ultra);
+        luxService.foodDelivery(ultra);
+        luxService.free(ultra);
+
+        System.out.println();
+
+        // попытка зарезервировать уже забронированную комнату
+        try {
+            service1.reserve(standard);
+            service1.reserve(standard);
+        } catch (RoomAlreadyReservedExc ex) {
+            System.out.println("ошибка: " + ex.getMessage());
+        }
 
     }
 }
